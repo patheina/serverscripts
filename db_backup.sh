64c6c2be5cd5f026c44afed31d08711f91ec8692
Alexander Richter <alexander.richter@atsonline.de>
2017-02-15 09:38:26 +0100

    add a opt parser for more flexibility

--- db_backup.sh
+++ db_backup.sh
@@ -1,8 +1,47 @@
- #!/bin/sh
+#!/bin/bash
 
-FILENAME="mysql_backup_"$(date +"%Y-%m-%d_%H-%M-%S")".sql";
-FILEPATH="/tmp/";
-GDRIVEPATH="Tardis-Backup";
+FILENAME="mysql_backup_"$(date +"%Y-%m-%d_%H-%M-%S")".sql"
+FILEPATH="/tmp/"
+GDRIVEPATH="Tardis-Backup"
+PRINT_HELP=0
+DATABASENAME="phe"
+
+
+##### Arguments
+GIVEN_ARGS=$#
+
+while getopts p:G:d:h opt; do
+    case $opt in
+	p)
+		FILEPATH=${OPTARG}
+        ;;
+	G)
+		GDRIVEPATH=${OPTARG}
+        ;;
+    d)
+        DATABASENAME=${OPTARG}
+        ;;
+    h)
+        PRINT_HELP=1
+        ;;
+    *)
+        PRINT_HELP=1
+        ;;
+    esac
+done
+
+if [ $GIVEN_ARGS -eq 0 -o $PRINT_HELP -eq 1 ]; then
+    echo ""
+    echo "### Backup to Google Drive ###"
+    echo "usage: db_backup [-p FILEPATH] [-G GDRIVEPATH] [-h]"
+    echo ""
+    echo "-p [FILEPATH] path to archive"
+    echo "-G [GDRIVEPATH] path at google drive"
+    echo "-d [DATABASENAME] database name"
+    echo "-h print help"
+    echo ""
+    exit 1
+fi
 
 cd $FILEPATH;
 
@@ -14,7 +53,7 @@ GDRIVEFOLDERID=$(gdrive list --no-header | grep $GDRIVEPATH | grep dir | awk '{ 
         GDRIVEFOLDERID=$(gdrive list --no-header | grep $GDRIVEPATH | grep dir | awk '{ print $1}')
     fi
 
-mysqldump -ubackup phe > $FILENAME;
+mysqldump -ubackup $DATABASENAME > $FILENAME;
 
 tar -czPf $FILEPATH$FILENAME.tar.gz $FILEPATH$FILENAME;
 rm -f $FILEPATH$FILENAME;
