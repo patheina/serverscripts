a9b0da7a507d27371158aa1c5216ddee5c67f2e9
root <root@tardis.patrick-heina.de>
2017-02-15 08:21:48 +0100

    initial

---
+++ db_backup.sh
@@ -0,0 +1,20 @@
+FILENAME="mysql_backup_"$(date +"%Y-%m-%d_%H-%M-%S")".sql";
+FILEPATH="/tmp/";
+GDRIVEPATH="Tardis-Backup";
+
+cd $FILEPATH;
+
+
+#check remote backup folder exists on gdrive
+GDRIVEFOLDERID=$(gdrive list --no-header | grep $GDRIVEPATH | grep dir | awk '{ print $1}')
+    if [ -z "$GDRIVEFOLDERID" ]; then
+        gdrive mkdir $GDRIVEPATH
+        GDRIVEFOLDERID=$(gdrive list --no-header | grep $GDRIVEPATH | grep dir | awk '{ print $1}')
+    fi
+
+mysqldump -ubackup phe > $FILENAME;
+
+tar cfz $FILENAME.tar.gz $FILENAME;
+rm -f $FILENAME;
+
+gdrive upload --parent $GDRIVEFOLDERID --delete $FILENAME.tar.gz
---
+++ gdrivebackup.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+# Source: https://guides.wp-bullet.com
+# Author: Mike
+
+#define local path for backups
+BACKUPPATH="/tmp/backups"
+
+#define remote backup path
+BACKUPPATHREM="Backup-Tardiss"
+
+#path to WordPress installations
+SITESTORE="/var/www/wordpress1/wordpress"
+
+#date prefix
+DATEFORM=$(date +"%Y-%m-%d")
+
+#Days to retain
+DAYSKEEP=7
+
+#calculate days as filename prefix
+DAYSKEPT=$(date +"%Y-%m-%d" -d "-$DAYSKEEP days")
+
+#create array of sites based on folder names
+SITELIST=($(ls -lh $SITESTORE | awk '{print $9}'))
+
+#make sure the backup folder exists
+mkdir -p $BACKUPPATH
+
+#check remote backup folder exists on gdrive
+BACKUPSID=$(gdrive list --no-header | grep $BACKUPPATHREM | grep dir | awk '{ print $1}')
+    if [ -z "$BACKUPSID" ]; then
+        gdrive mkdir $BACKUPPATHREM
+        BACKUPSID=$(gdrive list --no-header | grep $BACKUPPATHREM | grep dir | awk '{ print $1}')
+    fi
+
+#start the loop
+for SITE in ${SITELIST[@]}; do
+    #delete old backup, get folder id and delete if exists
+    OLDBACKUP=$(gdrive list --no-header | grep $DAYSKEPT-$SITE | grep dir | awk '{ print $1}')
+    if [ ! -z "$OLDBACKUP" ]; then
+        gdrive delete $OLDBACKUP
+    fi 
+
+    # create the local backup folder if it doesn't exist
+    if [ ! -e $BACKUPPATH/$SITE ]; then
+        mkdir $BACKUPPATH/$SITE
+    fi
+
+    #entire the WordPress folder
+    cd $SITESTORE/$SITE
+  
+    #back up the WordPress folder
+    tar -czf $BACKUPPATH/$SITENAME/$SITE/$DATEFORM-$SITE.tar.gz .
+    #back up the WordPress database, compress and clean up
+    wp db export $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql --allow-root --skip-themes --skip-plugins
+    cat $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql | gzip > $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz
+    rm $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql
+    
+    #get current folder ID
+    SITEFOLDERID=$(gdrive list --no-header | grep $SITE | grep dir | awk '{ print $1}')
+
+    #create folder if doesn't exist
+    if [ -z "$SITEFOLDERID" ]; then
+        gdrive mkdir --parent $BACKUPSID $SITE
+        SITEFOLDERID=$(gdrive list --no-header | grep $SITE | grep dir | awk '{ print $1}')
+    fi
+
+    #upload WordPress tar
+    gdrive upload --parent $SITEFOLDERID --delete $BACKUPPATH/$SITE/$DATEFORM-$SITE.tar.gz
+    #upload wordpress database
+    gdrive upload --parent $SITEFOLDERID --delete $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz
+
+done
+
+#Fix permissions
+sudo chown -R www-data:www-data $SITESTORE
+sudo find $SITESTORE -type f -exec chmod 644 {} +
+sudo find $SITESTORE -type d -exec chmod 755 {} +
