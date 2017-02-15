0258694d09dab74267372afbbc2c8b2d7fcb4fca
root <root@tardis.patrick-heina.de>
2017-02-15 09:25:57 +0100

    changed tar-command

--- db_backup.sh
+++ db_backup.sh
@@ -4,8 +4,6 @@ FILENAME="mysql_backup_"$(date +"%Y-%m-%d_%H-%M-%S")".sql";
 FILEPATH="/tmp/";
 GDRIVEPATH="Tardis-Backup";
 
-cd $FILEPATH;
-
 
 #check remote backup folder exists on gdrive
 GDRIVEFOLDERID=$(gdrive list --no-header | grep $GDRIVEPATH | grep dir | awk '{ print $1}')
@@ -14,9 +12,9 @@ GDRIVEFOLDERID=$(gdrive list --no-header | grep $GDRIVEPATH | grep dir | awk '{ 
         GDRIVEFOLDERID=$(gdrive list --no-header | grep $GDRIVEPATH | grep dir | awk '{ print $1}')
     fi
 
-mysqldump -ubackup phe > $FILENAME;
+mysqldump -ubackup phe > $FILEPATH$FILENAME;
 
-tar -czPf $FILEPATH$FILENAME.tar.gz $FILEPATH$FILENAME;
+tar -C /tmp/ -czf $FILENAME.tar.gz $FILENAME;
 rm -f $FILEPATH$FILENAME;
 
 gdrive upload --parent $GDRIVEFOLDERID --delete $FILEPATH$FILENAME.tar.gz
